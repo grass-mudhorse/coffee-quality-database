33aabce485edafa673818a3aaa5dab98f9494571
grass-mudhorse <ningborong@gmail.com>
2020-03-21 09:48:32 +0000

    update on 3.21

--- data/test1.py
+++ data/test1.py
@@ -45,7 +45,7 @@ t2.head()
 
 t2.to_csv("./arabica_cleaned.csv")
 
-t3 = pd.read_csv("./arabica_cleaned.csv")
+t3 = pd.read_csv("./arabica_data_cleaned1.csv")
 g = t3['Total.Cup.Points'].groupby(t3['Processing.Method'])
 t3['Processing.Method'].value_counts()
 t3['Region'].value_counts()
@@ -82,5 +82,9 @@ t3['Certification.Contact'].value_counts()
 t4['Country.of.Origin'].value_counts()
 t4['In.Country.Partner'].value_counts()
 t3['In.Country.Partner'].value_counts()
-
+t3 = t3.dropna(subset=['Color'])
+t3.to_csv("./arabica_data_cleaned2")
+t5 = pd.read_csv("./arabica_data_cleaned2")
+t5 = t5.rename(columns = {'Country.of.Origin':'Country','In.Country.Partner':'Parter','Total.Cup.Points':'Score'})
+t5.to_csv('./arabica_cleaned2.csv')
 
